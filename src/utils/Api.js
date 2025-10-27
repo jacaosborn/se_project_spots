8c3d0f0527a100a22c28d78ad1748f300292c8d5
Jacob <jacaosborn@gmail.com>
2025-10-27 18:48:37 -0500

    Fix object issue in API arguments

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,5 +1,5 @@
 export default class Api {
-  constructor(baseUrl, headers) {
+  constructor({ baseUrl, headers }) {
     this._baseUrl = baseUrl;
     this._headers = headers;
   }
@@ -14,97 +14,61 @@ export default class Api {
   getAppInfo() {
     return Promise.all([this.getInitialCards(), this.getUserInfo()]);
   }
-  // ATTENTION CODE CHECKER:
-  // need assistance on this. When I change the url in the fetch to
-  // `${this._baseUrl}cards`, it breaks. I can't figure out how to fix it
-  // same thing happens with all other fetches. Something wrong with the api
-  // construction on index.js I assume, but I don't know what.
+
   getInitialCards() {
-    return fetch("https://around-api.en.tripleten-services.com/v1/cards", {
-      headers: {
-        authorization: "88f5c885-c80c-45a1-8fab-3e371eeed876",
-      },
+    console.log(this._baseUrl);
+    return fetch(`${this._baseUrl}cards`, {
+      headers: this._headers,
     }).then(this._checkResponse);
   }
 
   getUserInfo() {
-    return fetch(`https://around-api.en.tripleten-services.com/v1/users/me`, {
-      headers: {
-        authorization: "88f5c885-c80c-45a1-8fab-3e371eeed876",
-      },
+    return fetch(`${this._baseUrl}users/me`, {
+      headers: this._headers,
     }).then(this._checkResponse);
   }
   editUserInfo({ name, about }) {
-    return fetch(`https://around-api.en.tripleten-services.com/v1/users/me`, {
+    return fetch(`${this._baseUrl}users/me`, {
       method: "PATCH",
-      headers: {
-        authorization: "88f5c885-c80c-45a1-8fab-3e371eeed876",
-        "Content-Type": "application/JSON",
-      },
+      headers: this._headers,
 
       body: JSON.stringify({ name, about }),
     }).then(this._checkResponse);
   }
   addNewPhoto({ name, link }) {
-    return fetch(`https://around-api.en.tripleten-services.com/v1/cards`, {
+    return fetch(`${this._baseUrl}cards`, {
       method: "POST",
-      headers: {
-        authorization: "88f5c885-c80c-45a1-8fab-3e371eeed876",
-        "Content-Type": "application/JSON",
-      },
+      headers: this._headers,
 
       body: JSON.stringify({ name, link }),
     }).then(this._checkResponse);
   }
 
   removeCard(cardId) {
-    return fetch(
-      `https://around-api.en.tripleten-services.com/v1/cards/${cardId}`,
-      {
-        method: "DELETE",
-        headers: {
-          authorization: "88f5c885-c80c-45a1-8fab-3e371eeed876",
-        },
-      }
-    ).then(this._checkResponse);
+    return fetch(`${this._baseUrl}cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._checkResponse);
   }
   addLike(cardId) {
-    return fetch(
-      `https://around-api.en.tripleten-services.com/v1/cards/${cardId}/likes`,
-      {
-        method: "PUT",
-        headers: {
-          authorization: "88f5c885-c80c-45a1-8fab-3e371eeed876",
-          "Content-Type": "application/JSON",
-        },
-      }
-    ).then(this._checkResponse);
+    return fetch(`${this._baseUrl}cards/${cardId}/likes`, {
+      method: "PUT",
+      headers: this._headers,
+    }).then(this._checkResponse);
   }
 
   removeLike(cardId) {
-    return fetch(
-      `https://around-api.en.tripleten-services.com/v1/cards/${cardId}/likes`,
-      {
-        method: "DELETE",
-        headers: {
-          authorization: "88f5c885-c80c-45a1-8fab-3e371eeed876",
-          "Content-Type": "application/JSON",
-        },
-      }
-    ).then(this._checkResponse);
+    return fetch(`${this._baseUrl}cards/${cardId}/likes`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._checkResponse);
   }
 
   editAvatar(avatar) {
-    return fetch(
-      `https://around-api.en.tripleten-services.com/v1/users/me/avatar`,
-      {
-        method: "PATCH",
-        headers: {
-          authorization: "88f5c885-c80c-45a1-8fab-3e371eeed876",
-          "Content-Type": "application/JSON",
-        },
-        body: JSON.stringify({ avatar }),
-      }
-    ).then(this._checkResponse);
+    return fetch(`${this._baseUrl}users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({ avatar }),
+    }).then(this._checkResponse);
   }
 }
